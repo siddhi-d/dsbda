53f4fc453bc127090377334f59346ec7b18308d2
siddhi <siddhideshmukh06@gmail.com>
2025-05-06 22:28:55 +0530

    Create wordcount.java

---
+++ wordcount.java
@@ -0,0 +1,86 @@
+﻿Word count 
+
+mapper
+package finalsiddhiword;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+
+public class word_map extends Mapper<Object, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] words = line.split("\\s+");
+        for (String w : words) {
+            if (!w.isEmpty()) {
+                word.set(w);
+                context.write(word, one);  // Emit all words
+            }
+        }
+    }
+}
+
+
+
+reducer
+package finalsiddhiword;
+import java.io.IOException;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+public class word_reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+
+        context.write(key, new IntWritable(sum));  // Emit each word with count
+    }
+}
+
+
+
+driver
+package finalsiddhiword;
+
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.io.Text;
+
+public class word_driver {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+
+        if (args.length != 2) {
+            System.err.println("Usage: sid_worddriver <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Word Count");
+
+        job.setJarByClass(word_driver.class);
+        job.setMapperClass(word_map.class);
+        job.setReducerClass(word_reduce.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+}
